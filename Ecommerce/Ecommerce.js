1d3e4026a22a544a3df07a750d181ee3d4e83cc8
HermannRienzner <rienznerhermann@gmail.com>
2026-04-24 08:56:18 +0200

    git commit

--- Ecommerce/Ecommerce.js
+++ Ecommerce/Ecommerce.js
@@ -1,20 +1,4 @@
-let righe = 0;
-let colonne = 0;
-let tr;
-let td;
-let p;
-function creaTabella(){
-  colonne = document.getElementById("getColonne").value;
-  righe = document.getElementById("getRighe").value;
-
-  let tabella = document.createElement(table);
-  for(let i = 0; i < righe; i ++){
-    tr = document.createElement("tr");
-    for(let j = 0; j < colonne; j++){
-      td = document.createElement("td");
-      td.innerHTML = "skdjfasjdhfshf"
-      tr.appendChild(td);
-    }
-    tabella.appendChild(tr);
-  }
-}
+let collection = [];
+let utente = {
+        nome: "", cognome: "", cap: 0, indirizzo: "", email: "", password: ""
+      }
